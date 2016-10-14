21e42a8ff64ec3e77338743c72b9c5eb0b95c82a
Miguel g. Ledesma <miguel.gledesma.duarte@gmail.com>
2016-10-14 18:15:21 -0500

    Se agrega valida la precencia de los campos

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
-  '''
-   Se crea el modelo con el generador rails g model Article
-    tittle:string body:text visits_count:integer
-  '''
+  # Se crea el modelo con el generador rails g model Article
+  # tittle:string body:text visits_count:integer
+   
+  #Validar el titulo
+  validates :tittle, presence: true
+  validates :body, presence: true
 end
