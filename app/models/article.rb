a7ba63775449525c4121363f594d1f0d9e87d45e
Miguel g. Ledesma <miguel.gledesma.duarte@gmail.com>
2016-10-14 19:38:52 -0500

    Agregamos mas validaciones

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,10 @@ class Article < ActiveRecord::Base
   # Se crea el modelo con el generador rails g model Article
   # tittle:string body:text visits_count:integer
    
-  #Validar el titulo
-  validates :tittle, presence: true
-  validates :body, presence: true
+  #Validar el titulo, que el titulo sea unico
+  validates :tittle, presence: true, uniqueness: true
+  # Cuerpo presente, largo minimo 20 caracteres
+  
+  validates :body, presence: true, length: { minimum: 20}    #validates :username, format: {whith: /regex} ejemplo validacion expresion regular
+ 
 end
